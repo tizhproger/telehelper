3d7bdaab598b8922f02c1c822823db7bcd99a48e
tizhproger <35668044+tizhproger@users.noreply.github.com>
2022-08-12 19:37:06 +0300

    Update personal_assistant.py

--- personal_assistant.py
+++ personal_assistant.py
@@ -654,7 +654,7 @@ async def commands(message):
 @client.on(events.NewMessage(outgoing=True))
 async def outgoing(event):
     msg = event.message
-    Check_db()
+    Check_db(msg)
     if msg.is_reply:
         id = (await event.message.get_reply_message()).sender.id
         if msg.text.startswith('.add'):
